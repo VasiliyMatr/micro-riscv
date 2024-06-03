0b845110082809a8976aa40aca98bb7cbc7025b5
VasiliyMatr <vasiliymatrenin@gmail.com>
2024-06-03 03:09:25 +0300

    [defines][memory] Add memory module
    - Add memory-related defines
    - Add memory module

--- src/defines.sv
+++ src/defines.sv
@@ -1,10 +1,15 @@
 // Contains common arch defines
 
+// Data sizes
 `define BYTE_BITS 8
 `define HALF_BITS 16
 `define WORD_BITS 32
+`define DWORD_BITS 64
 
-`define GPR_BITS `WORD_BITS
-
+// RISCV arch have 32 GPRs
+`define GPR_BITS `DWORD_BITS
 `define GPR_ID_BITS 5
 `define GPR_NUM (1 << `GPR_ID_BITS)
+
+// Memory access type is encoded with 3 bits
+`define FUNCT3_BITS 3
